4405b624e7baf742b9ecd04d4481ce58e2d8225c
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-18 14:54:15 +0600

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        System.out.println(number%10+number/10%10+number/100%10+number/1000);
     }
 }
