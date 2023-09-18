a1cb10b1b53b3baf19505e693d56d9a628228004
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-18 15:14:27 +0600

    Update DivisionByZero.java

--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -3,6 +3,6 @@ package lang.print.gaps.finalModuleTask;
 public class DivisionByZero {
     public static void main(String[] args) {
         double number = 10;
-        System.out.printlf(number/0);
+        System.out.println(number/0);
     }
 }
